f139214e2f31f7e8a58e17c2bd2f8a61d2aeb53b
Mahesh Rachha <maheshrachha1225@gmail.com>
2020-08-25 18:33:46 +0530

    initial

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+apiKey: "AIzaSyBYq2i-yRlMUoFlIyWyanmrnsZa-_vtFt0",
+  authDomain: "confusionserver-c5590.firebaseapp.com",
+  databaseURL: "https://confusionserver-c5590.firebaseio.com",
+  projectId: "confusionserver-c5590",
+  storageBucket: "confusionserver-c5590.appspot.com",
+  messagingSenderId: "497215698627",
+  appId: "1:497215698627:web:80c201fd8ccff58d0c1476"
+};
